1d616d011e1e3528a549014cefe5a1e28291590d
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2022-12-07 22:49:09 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="body-container">
+        <div className="profile-container">
+          <img clasName="circle" src="svds.jfif" width="100" height="100" alt="profile" />
+          <h2 id="portfolio-text-title">Sietze van de Star</h2>
+        </div>
+        <p id="portfolio-text-body">Hi 👋 I am a front-end engineer working on accesible digital experiences.</p>
+        <p id="portfolio-text-footer">Connect with me on: </p>
+        <div className="chip-container">
+          <div class="chip chip--green-medium"><a href='https://www.linkedin.com/in/sietze-van-de-star/' target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
+          <div class="chip chip--green-medium"><a href='https://codepen.io/svandes' target="_blank" rel="noopener noreferrer">Codepen</a></div>
+          <div class="chip chip--green-medium"><a href='https://github.com/op2020-cyber' target="_blank" rel="noopener noreferrer">GitHub</a></div>
+        </div>
+      </div>
     </div>
   );
 }
