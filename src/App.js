9a40bf71ab662af5b96767f13172ff3e666511da
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2022-12-07 23:35:06 +0100

    chip edits

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
       <div className="body-container">
         <div className="profile-container">
           <img clasName="circle" src="svds.jfif" width="100" height="100" alt="profile" />
-          <h2 id="portfolio-text-title">Sietze van de Star</h2>
+          <h2 id="portfolio-text-title">Sietze v.d. Star</h2>
         </div>
         <p id="portfolio-text-body">Hi 👋 I am a front-end engineer working on accesible digital experiences.</p>
         <p id="portfolio-text-footer">Connect with me on: </p>
         <div className="chip-container">
-          <div class="chip chip--green-medium"><a href='https://www.linkedin.com/in/sietze-van-de-star/' target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
-          <div class="chip chip--green-medium"><a href='https://codepen.io/svandes' target="_blank" rel="noopener noreferrer">Codepen</a></div>
-          <div class="chip chip--green-medium"><a href='https://github.com/op2020-cyber' target="_blank" rel="noopener noreferrer">GitHub</a></div>
+          <div class="chip chip--green"><a href='https://www.linkedin.com/in/sietze-van-de-star/' className="chip__a" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
+          <div class="chip chip--green"><a href='https://codepen.io/svandes' className="chip__a" target="_blank" rel="noopener noreferrer">Codepen</a></div>
+          <div class="chip chip--green"><a href='https://github.com/op2020-cyber' className="chip__a" target="_blank" rel="noopener noreferrer">GitHub</a></div>
         </div>
       </div>
     </div>
