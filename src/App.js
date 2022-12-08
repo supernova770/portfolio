f7682fdc07b5984a5e029cb90ddc4e42abfdb2ea
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2022-12-08 22:14:23 +0100

    changed font size title

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
           <img clasName="circle-img" src="svds.jfif" width="100" height="100" alt="profile" />
           <h2 id="portfolio-text-title">Sietze v.d. Star</h2>
         </div>
-        <p id="portfolio-text-body">Hi 👋 I am a front-end engineer working on accessible digital experiences.</p>
+        <p id="portfolio-text-body">Hi 👋 I am a front-end engineer and information designer working on interactive digital experiences.</p>
         <p id="portfolio-text-footer">Connect with me on: </p>
         <div className="chip-container">
           <div class="chip chip--green"><a href='https://www.linkedin.com/in/sietze-van-de-star/' className="chip__a" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
