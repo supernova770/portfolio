204fca7a0f00659621478f1787e5db1c086507ee
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2023-02-07 18:14:38 +0100

    fixed button

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
           <img clasName="circle-img" src="svds.jfif" width="100" height="100" alt="profile" />
           <h2 id="portfolio-text-title">Sietze v.d. Star</h2>
         </div>
-        <p id="portfolio-text-body">Hi 👋 I am a front-end engineer and data visualization designer working on human centered interactive digital experiences.</p>
-        <p id="portfolio-text-footer">Connect with me on: </p>
+        <p id="portfolio-text-body">Hi 👋 I am a front-end engineer and data visualization designer working on human-centered interactive digital experiences.</p>
+        <p id="portfolio-text-footer">Connect with me on:</p>
         <div className="chip-container">
           <div class="chip chip--green"><a href='https://www.linkedin.com/in/sietze-van-de-star/' className="chip__a" target="_blank" rel="noopener noreferrer">LinkedIn</a></div>
           <div class="chip chip--green"><a href='https://codepen.io/svandes' className="chip__a" target="_blank" rel="noopener noreferrer">Codepen</a></div>
