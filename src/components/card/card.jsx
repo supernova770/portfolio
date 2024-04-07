2580d0b0952783a5db5dc56cd9faa167c026f1c4
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2024-04-08 00:12:12 +0200

    new edits

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,21 +1,46 @@
-import {React} from 'react';
-import "./card.css"
-import { Chip } from '../chip';
+import { React, useState } from "react";
+import "./card.css";
+import { Chip } from "../chip";
 
-export const Card = ({href, name}) => {
-    return (
-        <div className="card">
-            <div className='innerCard'>
-            <h2 id="portfolio-text-title">Hi there!</h2>
-            <p id="portfolio-text-body">My name is Sietze. I am working on user-centered experiences with my UX design and frontend development skills.</p>
-            <div className="chip-container">
-                <Chip name={"LinkedIn"} href={'https://www.linkedin.com/in/sietze-van-de-star/'}/>
-                <Chip name={"Behance"} href={'https://www.behance.net/sietzevanda20e'}/>
-                <Chip name={"Codepen"} href={'https://codepen.io/svandes'}/>
-                <Chip name={"GitHub"} href={'https://github.com/supernova770'}/>
-                <Chip name={"React DDL | WIP"} href={'https://supernova770.github.io/dropdown-showcase/'}/>
-            </div>
-            </div>
+export const Card = ({ href, name }) => {
+  const [textShouldExtend, setTextShouldExtend] = useState(false)
+  function handleTextExtend() {
+    setTextShouldExtend(prevState => !prevState);
+  }
+  return (
+    <div className="card">
+      <div className="innerCard">
+        <h2 id="portfolio-text-title">Hi there!</h2>
+        <div id="portfolio-text-container">
+          <p id="portfolio-text-body">
+            My name is Sietze. I am working on user-centered experiences with my
+            diverse UX design and frontend development skills.
+          </p>
+          <div id={textShouldExtend ? "portfolio-text-extended-active" : "portfolio-text-extended-inactive"}>
+            I believe that design can solve problems in a way that is both
+            pleasing and impactful adressing both human needs and business constraints. 
+            This is achieved through a swift meticulous process of problem analysis, innovative solution 
+            exploration and prototyping, incorporation of user feedback, and the deliberate implementation of
+            design choices rooted in sound logical reasoning. 
+          </div>
         </div>
-    )
-}
+        <p onClick={handleTextExtend} id="button-extend">
+          {textShouldExtend ? "read less" : "read more..."}
+        </p>
+        <hr id="text-hr" />
+        <div className="chip-list">
+          <Chip
+            name={"Behance"}
+            href={"https://www.behance.net/sietzevanda20e"}
+          />
+          <Chip name={"Codepen"} href={"https://codepen.io/svandes"} />
+          <Chip name={"GitHub"} href={"https://github.com/supernova770"} />
+          <Chip
+            name={"React DDL | WIP"}
+            href={"https://supernova770.github.io/dropdown-showcase/"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/svgAnimationEngine/run.jsx
+++ src/svgAnimationEngine/run.jsx
@@ -1,16 +1,17 @@
-import { React, useEffect, useRef } from "react";
+import { React, useLayoutEffect, useRef } from "react";
 import _ from "lodash";
 
 let height = window.innerHeight,
   width = window.innerWidth;
 
 export const ParticleAnimation = () => {
+  //useRef to prevent problems with React.
   const partRef = useRef(null);
-
-  useEffect(() => {
-    //useRef to prevent problems with React.
-    let svg = partRef.current;
   
+  useLayoutEffect(() => {
+    
+    let svg = partRef.current,
+    colors = ["#f9f3bd", "#034ff4", "#050f1e"];
     const createSvgNode = (n, attributes) => {
       n = document.createElementNS("http://www.w3.org/2000/svg", n);
       for (let a in attributes) {
@@ -24,26 +25,28 @@ export const ParticleAnimation = () => {
       return n;
     };
 
-    let colors = ["#f9f3bd", "#034ff4", "#050f1e"];
-    class Particle {
+    class Circle {
       constructor() {
         this.r = _.random(1, 5);
         this.d = this.r * 2;
         this.x = _.random(this.r, width - this.d);
         this.y = _.random(this.r, height - this.d);
         this.col = colors[_.random(0, 3)];
-        this.mass = 10 * this.r;
-
+        this.vX = _.random(-10, 10) / 15;
+        this.vY = _.random(-10, 10) / 15;
         this.el = createSvgNode("circle", {
           cx: this.x,
           cy: this.y,
           r: this.r,
           fill: this.col,
         });
-
-        this.vX = _.random(-10, 10) / 15;
-        this.vY = _.random(-10, 10) / 15;
       }
+
+      draw() {
+        this.el.setAttribute("cx", this.x);
+        this.el.setAttribute("cy", this.y);
+      }
+
       update() {
         this.x += this.vX;
         this.y += this.vY;
@@ -57,18 +60,13 @@ export const ParticleAnimation = () => {
           this.y = this.y <= this.r ? this.r : height - this.r;
           this.vY *= -1;
         }
-
         this.draw();
-      }
-      draw() {
-        this.el.setAttribute("cx", this.x);
-        this.el.setAttribute("cy", this.y);
       }
     }
 
     // create particles
     let particles = [];
-    let maxParts = 25;
+    let maxParts = 24;
 
     if (width < 350) {
       maxParts = 10;
@@ -76,7 +74,7 @@ export const ParticleAnimation = () => {
 
     function createParticles() {
       for (let i = 0; i < maxParts; i++) {
-        particles.push(new Particle());
+        particles.push(new Circle());
       }
     }
 
@@ -92,6 +90,7 @@ export const ParticleAnimation = () => {
 
     //Start movement
     loop();
+    
   }, []);
 
   return (
