655d38f5e14c3612eef3c7910ed7e9ff19614829
op2020-cyber <66179409+op2020-cyber@users.noreply.github.com>
2024-04-07 11:04:13 +0200

    new edits

--- src/svgAnimationEngine/run.jsx
+++ src/svgAnimationEngine/run.jsx
@@ -1,7 +1,6 @@
 import { React, useEffect, useRef } from "react";
 import _ from "lodash";
 
-// view dimensions
 let height = window.innerHeight,
   width = window.innerWidth;
 
@@ -9,107 +8,89 @@ export const ParticleAnimation = () => {
   const partRef = useRef(null);
 
   useEffect(() => {
-	//useRef to prevent problems with React.
+    //useRef to prevent problems with React.
     let svg = partRef.current;
-    const setSvgSize = (w, h) => {
-      svg.setAttribute("width", width);
-      svg.setAttribute("height", height);
-    };
-
-    // function to create svg nodes
-    const createNode = (n, attrs) => {
+  
+    const createSvgNode = (n, attributes) => {
       n = document.createElementNS("http://www.w3.org/2000/svg", n);
-      for (let a in attrs) {
+      for (let a in attributes) {
         n.setAttributeNS(
           null,
           a.replace(/[A-Z]/g, (m) => "-" + m.toLowerCase()),
-          attrs[a]
+          attributes[a]
         );
       }
       svg.appendChild(n);
       return n;
     };
-	function getRandomInt(min, max) {
-		min = Math.ceil(min);
-		max = Math.floor(max);
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-    // particle constructor
-	let colors = ["#f9f3bd", "#034ff4", "#050f1e"]
 
+    let colors = ["#f9f3bd", "#034ff4", "#050f1e"];
     class Particle {
       constructor() {
         this.r = _.random(1, 5);
         this.d = this.r * 2;
         this.x = _.random(this.r, width - this.d);
         this.y = _.random(this.r, height - this.d);
-        this.col = colors[getRandomInt(0, 3)];
+        this.col = colors[_.random(0, 3)];
         this.mass = 10 * this.r;
-        // create el
-        this.el = createNode("circle", {
+
+        this.el = createSvgNode("circle", {
           cx: this.x,
           cy: this.y,
           r: this.r,
           fill: this.col,
         });
-        // vector
-        this.vx = _.random(-10, 10) / 15;
-        this.vy = _.random(-10, 10) / 15;
-      }
-      get vector() {
-        return [this.vx, this.vy];
+
+        this.vX = _.random(-10, 10) / 15;
+        this.vY = _.random(-10, 10) / 15;
       }
       update() {
-        this.x += this.vx;
-        this.y += this.vy;
+        this.x += this.vX;
+        this.y += this.vY;
+
         // Outside bounderies
         if (this.x <= this.r || this.x + this.r > width) {
           this.x = this.x <= this.r ? this.r : width - this.r;
-          this.vx *= -1;
+          this.vX *= -1;
         }
         if (this.y <= this.r || this.y + this.r > height) {
           this.y = this.y <= this.r ? this.r : height - this.r;
-          this.vy *= -1;
+          this.vY *= -1;
         }
-        // draw and update the position
+
         this.draw();
       }
       draw() {
         this.el.setAttribute("cx", this.x);
         this.el.setAttribute("cy", this.y);
-        this.collision = false;
       }
     }
 
     // create particles
     let particles = [];
-    let maxParticles = 25;
+    let maxParts = 25;
 
-	if(width < 350) {
-		maxParticles = 10
-	}
+    if (width < 350) {
+      maxParts = 10;
+    }
 
     function createParticles() {
-      for (let i = 0; i < maxParticles; i++) {
+      for (let i = 0; i < maxParts; i++) {
         particles.push(new Particle());
       }
     }
 
     // animation loop
     const loop = () => {
-      // page resizing
-      width = window.innerWidth;
-      height = window.innerHeight;
-      setSvgSize(width, height);
-      // looping through particles and update position
-      for (let i = 0; i < maxParticles; i++) {
+      for (let i = 0; i < maxParts; i++) {
         particles[i].update();
       }
       window.requestAnimationFrame(loop);
     };
 
     createParticles();
-	//Start movement
+
+    //Start movement
     loop();
   }, []);
 
